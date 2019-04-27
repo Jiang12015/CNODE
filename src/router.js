3caa2c244a939a14a4d978b273a97361271196dd
jiang <421524079@qq.com>
2019-04-28 01:57:42 +0800

    修正

--- src/router.js
+++ src/router.js
@@ -7,8 +7,6 @@ import PostList from './components/PostList'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [{
     name: 'root',
     path:'/',
